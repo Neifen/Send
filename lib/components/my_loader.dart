8c4c7c6b0df83d2372494554fd522ee1e37baf8e
Nathan <nate.bourquin@gmail.com>
2023-06-30 20:52:18 -0500

    add changes

--- lib/components/my_loader.dart
+++ lib/components/my_loader.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MyLoader extends StatelessWidget {
   final Future future;
-  final Widget child;
   final Widget? alternative;
-  const MyLoader({super.key, required this.future, required this.child, this.alternative});
+  final Function(BuildContext, AsyncSnapshot<dynamic>) builder;
+  const MyLoader({super.key, required this.future, required this.builder, this.alternative});
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: future,
-      builder: (context, snapshot) => snapshot.hasData ? child : alternative ?? const CircularProgressIndicator(),
+      builder: (context, snapshot) => snapshot.hasData ? Builder(builder: builder(context, snapshot)) : alternative ?? const CircularProgressIndicator(),
     );
     ;
   }
--- lib/pages/overview.dart
+++ lib/pages/overview.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:send/components/my_loader.dart';
 import 'package:send/pages/route_page.dart';
@@ -11,50 +10,24 @@ class OverviewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final Future<List> list = OverviewService("V0").getById("id");
 
-
-    return MyLoader(future: list, child: ListView.builder(
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: ((context, index) {
-
-                        var route = snapshot.data?[index];
-                        return OutlinedButton(
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (_) => RoutePage(route)));
-                            },
-                            child: Card(
-                              child: ListTile(
-                                  title: Text('here come item #$index'),
-                                  subtitle: Text('test: ${route['ranking']}'),
-                                  leading: Container(
-                                    child: Text("Here comes a picture"),
-                                  )),
-                            ));
-                      })))
-
-    return FutureBuilder(
+    return MyLoader(
         future: list,
-        builder: (context, snapshot) => Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: snapshot.hasData
-                  ? ListView.builder(
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: ((context, index) {
-
-                        var route = snapshot.data?[index];
-                        return OutlinedButton(
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (_) => RoutePage(route)));
-                            },
-                            child: Card(
-                              child: ListTile(
-                                  title: Text('here come item #$index'),
-                                  subtitle: Text('test: ${route['ranking']}'),
-                                  leading: Container(
-                                    child: Text("Here comes a picture"),
-                                  )),
-                            ));
-                      }))
-                  : Container(),
-            ));
+        builder: (_, snapshot) => ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: ((context, index) {
+              var route = snapshot.data?[index];
+              return OutlinedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => RoutePage(route)));
+                  },
+                  child: Card(
+                    child: ListTile(
+                        title: Text('here come item #$index'),
+                        subtitle: Text('test: ${route['ranking']}'),
+                        leading: Container(
+                          child: Text("Here comes a picture"),
+                        )),
+                  ));
+            })));
   }
 }
